de58c044f8385e81c34636ae29d733599891aee5
Kunzst <sk193@web.de>
2018-10-31 14:03:15 +0100

    Fix names of  columns

--- AST_preprocessing.R
+++ AST_preprocessing.R
@@ -37,9 +37,34 @@ AST_trait_DB[, `:=`(Species = lapply(Genus_and_species, function(y) {
 )]
 
 
+# compare Unresolved_taxa column with Genus column with regard to sp. and spp. 
+# AST_trait_DB[Unresolved_taxa %like% "sp.$|spp.$", (lapply(.SD, function(y) ifelse(is.na(y), Unresolved_taxa,
+#                                                                                        as.character(y)))), 
+#              .SDcols = "Genus"]
+
+
 # Delete Genus_and_species column
 AST_trait_DB[, Genus_and_species := NULL]
 
 
 # set order of columns
 setcolorder(AST_trait_DB, c("Order", "Family", "Genus", "Species", "long_code", "short_code","Unresolved_taxa"))
+
+
+# Some colnames contian "." or ".." -> needs to be changed
+setnames(x = AST_trait_DB, old = names(AST_trait_DB), 
+         new = gsub(pattern = "\\.", replacement = "_", x = names(AST_trait_DB)))
+
+
+# still some colnames have two "_" 
+setnames(x = AST_trait_DB, old = names(AST_trait_DB), 
+         new = gsub(pattern = "\\__", replacement = "_", x = names(AST_trait_DB)))
+
+
+# Some columns have list as type -> change to write table
+grep("list", sapply(AST_trait_DB, typeof), value = TRUE)
+
+
+# write
+write.table(x = AST_trait_DB, file = "/home/kunz/Dokumente/Trait DB/Australian/Australian_macroinv_trait_DB_upd.csv",
+            sep = ",", dec = ".")
